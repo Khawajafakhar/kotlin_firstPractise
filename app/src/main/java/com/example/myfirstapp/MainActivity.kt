ee060282934ba2dd2244d8d28adf7cf905149b3f
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-04-06 11:51:28 +0500

    Added counter with button and textView

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val btnClickMe = findViewById<Button>(R.id.button)
+       val txtView = findViewById<TextView>(R.id.textView)
+       var totalNumber = 0
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "Add More"
+            totalNumber += 1
+            txtView.text = totalNumber.toString()
+            Toast.makeText(this, "Total Numbers are$totalNumber",Toast.LENGTH_SHORT).show()
+        }
     }
 }
