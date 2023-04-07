6062f101fc0ddd430a7c1d3f2ee6f87c89e85853
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-04-07 12:48:38 +0500

    data class in kotlin and a challange

--- app/src/main/java/com/example/myfirstapp/main.kt
+++ app/src/main/java/com/example/myfirstapp/main.kt
@@ -10,8 +10,18 @@ fun main(){
     println("heigth of Saad is ${saad.height}")
     saad.height =5.6
     println("Height updated ${saad.height}")
+   val user1 = User(1,"Fakhar")
+    println("Data class user ${user1.name}")
+    user1.name ="Saad"
+    println("Data class user name updated ${user1.name}")
+    println("User details : $user1")
+
+    apple.chargeBattery(30);
 
 }
+
+//We can also create a data class
+data class User(val id:Int,var name:String)
 
 class Person(firstName: String = "Saad", lastName: String = "Malik"){
    lateinit var age: String
@@ -31,8 +41,16 @@ class Person(firstName: String = "Saad", lastName: String = "Malik"){
 }
 
 class MobilePhone(osName: String ="Android",brand: String ="Samsung",model: String = "Galaxy S20 Ultra"){
+   var battery: Int = 45
+
     init {
         println("Mobile Phone $osName $brand $model")
 
+    }
+//Challenge solved
+    fun chargeBattery(battery: Int){
+        var batteryToBeCharged =battery
+        println("Phone had ${this.battery} battery and phone is charged by 30 and now it has ${this.battery + batteryToBeCharged}")
+        println(this.battery)
     }
 }
