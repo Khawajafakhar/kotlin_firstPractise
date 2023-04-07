e71cdba158f54b6390186e1816371ad0ec09af48
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-04-07 11:41:09 +0500

    lateint, setters and getters in a class

--- app/src/main/java/com/example/myfirstapp/main.kt
+++ app/src/main/java/com/example/myfirstapp/main.kt
@@ -4,15 +4,30 @@ fun main(){
   var fakhar = Person("Khawaja","Fakhar")
     var saad = Person()
     var samsung = MobilePhone();
-    var Apple = MobilePhone("IOs","Apple","Iphone 14")
+    var apple = MobilePhone("IOs","Apple","Iphone 14")
+    saad.age ="26"
+    println(saad.age)
+    println("heigth of Saad is ${saad.height}")
+    saad.height =5.6
+    println("Height updated ${saad.height}")
+
 }
 
 class Person(firstName: String = "Saad", lastName: String = "Malik"){
-
+   lateinit var age: String
     // init is an initializer block that will auto run when any object will be created
     init {
         println("Person created $firstName $lastName")
     }
+
+    //we can also create custom setter in the below format
+    var height: Double = 6.1
+        get() {
+            return field
+        }
+        set(value) {
+            field = if (value > 5.5) value else height
+        }
 }
 
 class MobilePhone(osName: String ="Android",brand: String ="Samsung",model: String = "Galaxy S20 Ultra"){
