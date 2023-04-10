69205e7d5c09b4be7ea6c78fa2fdcdbeadc3b6a1
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-04-10 13:01:06 +0500

    Lists in kotlin

--- app/src/main/java/com/example/myfirstapp/Basics.kt
+++ app/src/main/java/com/example/myfirstapp/Basics.kt
@@ -19,6 +19,34 @@ fun main(){
 
       val fruits = arrayOf(Fruit("apple",2.5),Fruit("Mango",3.5))
 
+    /*There are several differences between a list and an array
+       extra functionality to perform on list etc list.size
+       lists are immutable , we cannot update list until we make it a mutable list
+
+     */
+    val months = listOf("Jan","Feb","Mar")
+    println(months[1])
+    val anotherList = months.toMutableList()
+    val moreMonths = arrayOf("Apr,May,June")
+    anotherList.addAll(moreMonths)
+    println(anotherList)
+    anotherList.addAll(2,arrayOf("Mid Feb").toList())
+    println(anotherList)
+    anotherList.addAll(arrayOf("july","august","september","oct","Nov","Dec"))
+    println(anotherList)
+
+    /*
+    We can also make the list mutable when we create it
+     */
+
+    val days = mutableListOf<String>("Mon","tue","thur")
+    println("mutable list $days")
+    days.add("wed")
+    days[2] = "fri"
+    days.removeAt(0)
+    println("mutable list $days")
+
+
 }
 
 data class  Fruit(val name: String, val price: Double)
